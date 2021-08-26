28037b1785430ed0a65d47b00f1d33b8d6ee5150
Peter Mai <pm123633@gmail.com>
2021-08-26 17:37:11 -0500

    Add JavaScript function expression queries
    Add function expression query
    
    Trial of JS arrow function

--- queries/javascript/nvimGPS.scm
+++ queries/javascript/nvimGPS.scm
@@ -13,3 +13,22 @@
 ((method_definition
 	name: (property_identifier) @method-name
 	body: (statement_block)) @scope-root)
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> af44664 (Trial of JS arrow function)
+
+; Arrow Function
+((variable_declarator
+	name: (identifier) @function-name
+	value: (arrow_function)) @scope-root)
+<<<<<<< HEAD
+
+; Function Expression
+((variable_declarator
+	name: (identifier) @function-name
+	value: (function)) @scope-root)
+>>>>>>> dfb0c22 (Add function expression query)
+=======
+>>>>>>> af44664 (Trial of JS arrow function)
